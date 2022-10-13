165157518fa09093eecfafbaa717b24ffd6b94b1
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-10-13 11:01:34 +0530

    Time: 1 ms (63.72%), Space: 44.4 MB (15.40%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -22,11 +22,14 @@ class Solution {
         
         if(root == null) return true;
         
+        // If node value not within the range then return false
         if(root.val <= min || root.val >= max) return false;
         
+        // Update the range for next recursion cycle
         boolean leftValid = validity(root.left, min, (long) root.val);
         boolean rightValid = validity(root.right, (long) root.val, max);
         
+        // if both left and right node are valid then then return true
         return leftValid && rightValid;
         
     }
