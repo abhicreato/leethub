ec37dee0485162881fc2e758fee0ff2627b17d43
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-10-19 15:54:59 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.java
+++ Floyd Warshall - GFG/floyd-warshall.java
@@ -44,30 +44,30 @@ class Solution
         // Code here 
         int vertex = matrix.length;
 
-        for(int i=0;i<vertex;i++){
-            for( int j = 0; j<vertex; j++){
-                if(matrix[i][j] == -1) matrix[i][j] = 9999;
-            }
-        }
+        // for(int i=0;i<vertex;i++){
+        //     for( int j = 0; j<vertex; j++){
+        //         if(matrix[i][j] == -1) matrix[i][j] = 9999;
+        //     }
+        // }
         
         for(int k = 0 ; k < vertex ; k++){
             for(int i = 0; i < vertex ; i++){
                 for(int j = 0; j < vertex ; j++){
                     
-                    if(matrix[i][k] == 9999 || matrix[k][j] == 9999){
+                    if(matrix[i][k] == -1 || matrix[k][j] == -1){
                        continue; 
                     }else{
-                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                      matrix[i][j] = (matrix[i][j] == -1) ? matrix[i][k] + matrix[k][j] : Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
                     } 
  
                 }
             }
         }
         
-        for(int i=0;i<vertex;i++){
-            for( int j = 0; j<vertex; j++){
-                if(matrix[i][j] == 9999) matrix[i][j] = -1;
-            }
-        }
+        // for(int i=0;i<vertex;i++){
+        //     for( int j = 0; j<vertex; j++){
+        //         if(matrix[i][j] == 9999) matrix[i][j] = -1;
+        //     }
+        // }
     }
 }
