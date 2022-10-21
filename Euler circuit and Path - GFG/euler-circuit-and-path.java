98d7fef4d21bead6d15358defb6ee8edea986e6a
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-10-21 16:34:24 +0530

    Added solution - LeetHub

--- Euler circuit and Path - GFG/euler-circuit-and-path.java
+++ Euler circuit and Path - GFG/euler-circuit-and-path.java
@@ -46,6 +46,7 @@ class Solution
         int nodeWithEdge = 0;
         boolean allZeroDegree = true;
         
+        // Find node with edge
         for(int i=0; i<V; i++){
             if(adj.get(i).size() > 0){
                 nodeWithEdge = i;
@@ -53,35 +54,34 @@ class Solution
             }
         }
         
-
+        // Travese with that node 
+        dfs(nodeWithEdge,adj,visited);
             
-            dfs(nodeWithEdge,adj,visited);
-            
-            for(int i=0;i<V;i++){
-                
-                if(visited[i] == false && adj.get(i).size() > 0){
+        // If any node is notvisited and it has edge then not a Euler
+        for(int i=0;i<V;i++){
+            if(visited[i] == false && adj.get(i).size() > 0){
                     return 0;
-                }
             }
+        }
             
-            int oddDegree = 0;
+        // check for semi euler
+        int oddDegree = 0;
             
-            for(int i=0;i<V;i++){
-                if(adj.get(i).size() % 2 != 0)
-                oddDegree++;
-            }
+        for(int i=0;i<V;i++){
+            if(adj.get(i).size() % 2 != 0)
+            oddDegree++;
+        }
             
-            if(oddDegree == 0){
-                return 2;
-            }else if(oddDegree == 2){
-                return 1;
-            }else if(oddDegree > 2){
-                return 0;
-            }
-            
+        if(oddDegree == 0){
+            return 2;
+        }else if(oddDegree == 2){
+            return 1;
+        }else if(oddDegree > 2){
             return 0;
+        }
+            
+        return 0;
 
-    
     }
     
     public void dfs(int curr, ArrayList<ArrayList<Integer>> adj, boolean[] visited){
