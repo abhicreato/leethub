ee51f28a520d79a5645a82ea82ed32c0ed0e2671
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-10-25 11:24:32 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -57,13 +57,26 @@ class Solution
         
         for(int i=0;i<cost.length;i++){
             for(int j =0;j < cost[i].length;j++)
-            cost[i][j] = -1;
+            if(i == 0 || j == 0) cost[i][j] = 0; 
+            
         }
         
-        return solve(W,wt,val,n);
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < W + 1; j++){
+                if(j >= wt[i - 1]){
+                    cost[i][j] = Math.max(val[i - 1] + cost[i - 1][j - wt[i - 1]], cost[i - 1][j]);
+                }else{
+                    cost[i][j] = cost[i - 1][j];
+                }
+            }
+        } 
+        
+        return cost[n][W];
+        //return solve(W, wt,val,n);
 
     } 
     
+    //recursion soln
     static int solve(int W, int[] wt, int val[], int n){
         
         if(n == 0 || W == 0) return 0;
