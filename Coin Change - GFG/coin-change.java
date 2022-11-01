8d2da942719d018a39df5d908ff6ac0cc95b89ab
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-11-01 11:42:03 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.java
+++ Coin Change - GFG/coin-change.java
@@ -29,40 +29,32 @@ class Solution {
     public long count(int coins[], int N, int sum) {
         // code here.
         
-        dp = new long[N+1][sum + 1];
+        dp = new long[N + 1][sum + 1];
         
         for(int i=0;i<=N;i++){
             for(int j=0;j<=sum;j++){
                 dp[i][j] = -1;
             }
         }
-        
-        long max = 0;
-        
-        // for(int i = 0; i<N; i++){
-        //     max = Math.max(max,solve(i,sum,coins,0));
-        // }
-        
-        solve(N - 1 ,sum,coins,0);
+
+        solve(N - 1 , sum, coins);
         
         return dp[N - 1][sum];
 
     }
     
-    public long solve(int index, int sum, int[] coins, int ways){
+    public long solve(int index, int sum, int[] coins){
         
         if(index < 0 || sum < 0) return 0;
         
-        if(sum == 0){
-            return ++ways;
-        };
+        if(sum == 0) return 1;
         
         if(dp[index][sum] != -1) return dp[index][sum];
         
         if(sum >= coins[index]){
-            return dp[index][sum] = solve(index, sum - coins[index], coins, ways) + solve(index - 1, sum, coins,ways);
+            return dp[index][sum] = solve(index, sum - coins[index], coins) + solve(index - 1, sum, coins);
         }else{
-            return dp[index][sum] = solve(index - 1, sum, coins, ways);
+            return dp[index][sum] = solve(index - 1, sum, coins);
         }
         
     }
