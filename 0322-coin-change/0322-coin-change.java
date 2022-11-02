44a4928af7045bdce53097c3da3f82f90553d853
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-11-02 09:37:24 +0530

    Time: 49 ms (31.38%), Space: 58.8 MB (9.99%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -5,15 +5,43 @@ class Solution {
         
         int n = coins.length;
         
-        dp = new int[n+1][amount + 1];
+        dp = new int[n + 1][amount + 1];
         
         for(int i=0;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                if(i == 0) dp[i][j] = Integer.MAX_VALUE - 1;
+                if(j == 0) dp[i][j] = 0;
+                // if(i == 1){
+                //     if(j % coins[i] == 0){
+                //         dp[i][j] = j/coins[i];
+                //     }else{
+                //         dp[i][j] = Integer.MAX_VALUE - 1;
+                //     }
+                // }
+            }
+        }
+        
+        for(int i = 1; i<=n; i++){
+            for(int j = 1;j<=amount;j++){
+                
+                if(j >= coins[i-1]){
+                    dp[i][j] = Math.min(1 + dp[i][j - coins[i-1]], dp[i - 1][j]);
+                }else{
+                    dp[i][j] = dp[i - 1][j]; 
+                }
+                
+            }
+        }
+ 
+        int min = dp[n][amount];
+        
+        /*for(int i=0;i<=n;i++){
             for(int j=0;j<=amount;j++){
                 dp[i][j] = -1;
             }
         }
         
-        int min = solve(n - 1, amount, coins);
+        int min = solve(n - 1, amount, coins);*/
         
         if(min < Integer.MAX_VALUE - 1) return min;
         
