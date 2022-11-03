5f2d53c4f72bd610d00a195f6e6eb4bb1eda9b76
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-11-03 11:58:53 +0530

    Time: 20 ms (74.12%), Space: 46.2 MB (62.43%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -9,32 +9,32 @@ class Solution {
         
         dp = new int[l1 + 1][l2 + 1];
         
-        for(int i=0;i<=l1;i++){
-            for(int j=0;j<=l2;j++){
-                if(i==0 || j==0) dp[i][j] = 0; 
-            }
-        }
-        
-        for(int i= 1;i<=l1;i++){
-            for(int j=1;j<=l2;j++){
-                
-                if(text1.charAt(i-1) == text2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
-                }
-            }
-        }
-        
-        return dp[l1][l2];
-        
 //         for(int i=0;i<=l1;i++){
 //             for(int j=0;j<=l2;j++){
-//                 dp[i][j] = -1;
+//                 if(i==0 || j==0) dp[i][j] = 0; 
 //             }
 //         }
         
-//         return solve(text1,l1,text2,l2);
+//         for(int i= 1;i<=l1;i++){
+//             for(int j=1;j<=l2;j++){
+                
+//                 if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+//                     dp[i][j] = 1 + dp[i-1][j-1];
+//                 }else{
+//                     dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+//                 }
+//             }
+//         }
+        
+//         return dp[l1][l2];
+        
+        for(int i=0;i<=l1;i++){
+            for(int j=0;j<=l2;j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        return solve(text1,l1,text2,l2);
         
     }
     
@@ -44,10 +44,10 @@ class Solution {
         
         if(dp[l1][l2] != -1) return dp[l1][l2];
         
-        if(t1.charAt(l1 - 1) == t2.charAt(l2 - 1)){
-            return dp[l1][l2] =  1 + solve(t1.substring(0, l1 - 1), l1 - 1, t2.substring(0, l2 - 1),l2-1);
+        if(t1.charAt(l1-1) == t2.charAt(l2-1)){
+            return dp[l1][l2] =  1 + solve(t1, l1 - 1, t2, l2 - 1);
         }else{
-            return dp[l1][l2] = Math.max(solve(t1, l1, t2.substring(0, l2 - 1), l2 - 1), solve(t1.substring(0, l1 - 1),l1 - 1, t2, l2));
+            return dp[l1][l2] = Math.max(solve(t1, l1, t2, l2 - 1), solve(t1,l1 - 1, t2, l2));
         }
     }
 }
