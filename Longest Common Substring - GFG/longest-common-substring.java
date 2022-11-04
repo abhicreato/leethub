82376124668912cdaa89caf09d31bffa83beb1e0
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-11-04 10:11:00 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.java
+++ Longest Common Substring - GFG/longest-common-substring.java
@@ -30,46 +30,49 @@ class GFG
 //User function Template for Java
 
 class Solution{
+    
     int ans = 0;
     int [][] dp;
     
     int longestCommonSubstr(String S1, String S2, int n, int m){
         // code here
         dp = new int [n+1][m+1];
-        
+
+        // Memoazation
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=m;j++){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // solve(S1,S2,n,m);
+        // return ans;
+    
+        // Tabuation
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
-                dp[i][j] = -1;
+                if(i==0 || j==0) dp[i][j] = 0;
             }
         }
         
-        // for(int i=0;i<=n;i++){
-        //     for(int j=0;j<=m;j++){
-        //         if(i==0 || j==0) dp[i][j] = 0;
-        //     }
-        // }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(S1.charAt(i - 1) == S2.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
         
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=m;j++){
-        //         if(S1.charAt(i - 1) == S2.charAt(j - 1)){
-        //             dp[i][j] = 1 + dp[i-1][j-1];
-        //         }else{
-        //             dp[i][j] = 0;
-        //         }
-        //     }
-        // }
+        int max = 0;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                max = Math.max(max,dp[i][j]);
+            }
+        }
         
-        // int max = 0;
-        // for(int i=0;i<=n;i++){
-        //     for(int j=0;j<=m;j++){
-        //         max = Math.max(max,dp[i][j]);
-        //     }
-        // }
+        return max;
         
-        // return dp[n][m];
-        
-        solve(S1,S2,n,m);
-        return ans;
     }
     
     int solve(String s1,String s2,int n, int m){
@@ -89,4 +92,6 @@ class Solution{
             return dp[n][m] = 0;
         }
     }
+
+    
 }
