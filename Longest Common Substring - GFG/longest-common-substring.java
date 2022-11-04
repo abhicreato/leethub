9d9e5f59e613bc2333a1c5b99d83e4a2f4b4d788
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-11-04 10:13:05 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.java
+++ Longest Common Substring - GFG/longest-common-substring.java
@@ -39,39 +39,39 @@ class Solution{
         dp = new int [n+1][m+1];
 
         // Memoazation
-        // for(int i=0;i<=n;i++){
-        //     for(int j=0;j<=m;j++){
-        //         dp[i][j] = -1;
-        //     }
-        // }
-        // solve(S1,S2,n,m);
-        // return ans;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                dp[i][j] = -1;
+            }
+        }
+        solve(S1,S2,n,m);
+        return ans;
     
         // Tabuation
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(i==0 || j==0) dp[i][j] = 0;
-            }
-        }
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=m;j++){
+        //         if(i==0 || j==0) dp[i][j] = 0;
+        //     }
+        // }
         
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(S1.charAt(i - 1) == S2.charAt(j - 1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = 0;
-                }
-            }
-        }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(S1.charAt(i - 1) == S2.charAt(j - 1)){
+        //             dp[i][j] = 1 + dp[i-1][j-1];
+        //         }else{
+        //             dp[i][j] = 0;
+        //         }
+        //     }
+        // }
         
-        int max = 0;
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                max = Math.max(max,dp[i][j]);
-            }
-        }
+        // int max = 0;
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=m;j++){
+        //         max = Math.max(max,dp[i][j]);
+        //     }
+        // }
         
-        return max;
+        // return max;
         
     }
     
