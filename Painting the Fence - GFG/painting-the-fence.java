2bbf0dc1b64534628d261a3cae21fe868128282c
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-11-25 10:38:41 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.java
+++ Painting the Fence - GFG/painting-the-fence.java
@@ -16,9 +16,33 @@ class Solution
     long countWays(int n,int k)
     {
         //code here.
+        if(n == 0) return 0;
+        
+        if(n == 1) return k;
+        
         dp = new double[n+1];
-        for(int i=0;i<=n;i++) dp[i] = -1;
-        return (long) solve(n,k);
+        
+        // Memoazation
+        // for(int i=0;i<=n;i++) dp[i] = -1;
+        // return (long) solve(n,k);
+        
+        // tabulation
+
+        dp[0] = 0;
+        dp[1] = k;
+        dp[2] = k + (k * (k - 1));
+        
+        double twoSameColor = 0;
+        double twoDiffColor = 0;
+        
+        for(int i=3;i<=n;i++){
+            twoSameColor = dp[i - 2] * (k - 1);
+            twoDiffColor = dp[i - 1] * (k - 1);
+            dp[i] = (twoSameColor + twoDiffColor) % mod;
+        }
+        
+        return (long) dp[n];
+        
     }
     
     double solve(int n, int k){
