f5c58755c913d1406d5d1b21302e0f3dd8bd605b
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-11-26 16:17:46 +0530

    Added solution - LeetHub

--- LCS of three strings - GFG/lcs-of-three-strings.java
+++ LCS of three strings - GFG/lcs-of-three-strings.java
@@ -25,7 +25,7 @@ class GFG{
 
 //User function Template for Java
 class Solution 
-{ 
+{
     int dp[][][];
     int LCSof3(String A, String B, String C, int n1, int n2, int n3) 
     { 
@@ -54,7 +54,7 @@ class Solution
             
             int x = solve(A, B, C, n1 - 1, n2, n3);
             int y = solve(A, B, C, n1, n2 - 1, n3);
-            int z = solve(A, B, C, n1, n2 , n3 - 1);
+             int z = solve(A, B, C, n1, n2 , n3 - 1);
             
             return dp[n1][n2][n3] = Math.max(x, Math.max(y,z));
         }
