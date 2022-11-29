c9fdfbb62619edabb8f2633e1a475fc66962872a
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-11-29 08:32:37 +0530

    Time: 346 ms (16.79%), Space: 43 MB (86.39%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,18 +2,20 @@ class Solution {
     int dp[];
     public boolean canJump(int[] nums) {
         
-        // dp = new int[nums.length + 1];
-        // for(int i=0;i<=nums.length;i++) dp[i] = -1;
-        // return solve(nums, 0) == 1 ? true : false ;
+        //Memoization
+        dp = new int[nums.length + 1];
+        for(int i=0;i<=nums.length;i++) dp[i] = -1;
+        return solve(nums, 0) == 1 ? true : false ;
         
-        int canReach = 0;
-        for (int i = 0; i <= canReach; i++) {
-            canReach = Math.max(canReach, i + nums[i]);
+        // Greedy
+        //int canReach = 0;
+//         for (int i = 0; i <= canReach; i++) {
+//             canReach = Math.max(canReach, i + nums[i]);
             
-            if (canReach >= nums.length - 1) return true;
-        }
+//             if (canReach >= nums.length - 1) return true;
+//         }
         
-        return false;
+//         return false;
   
     }
     
@@ -27,7 +29,9 @@ class Solution {
         
         for(int i = 1 ; i <= nums[currIndex] ; i++){
             canReach =  solve(nums, currIndex + i);
+            
             if(canReach == 1) return dp[currIndex] = 1;
+            
         }
         
         return dp[currIndex] = 0;
