8a070c8058f017bc91c48923b83e3a4c8906024d
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-11-30 11:14:10 +0530

    Added solution - LeetHub

--- Minimum cost to fill given weight in a bag - GFG/minimum-cost-to-fill-given-weight-in-a-bag.java
+++ Minimum cost to fill given weight in a bag - GFG/minimum-cost-to-fill-given-weight-in-a-bag.java
@@ -26,6 +26,7 @@ class GfG
 
 class Solution
 {
+
     int dp[][];
 	public int minimumCost(int cost[], int N, int W)
 	{
@@ -40,26 +41,20 @@ class Solution
 	   return  op < 0 ? -1 : op;
 	}
 	
-	public int solve(int cost[], int n, int w){
+	public int solve(int cost[], int N, int W){
 	    
-	    if(w == 0) return 0;
+	    if(W == 0) return 0;
 	    
-	    if(n == 0) return 100000;
+	    if(N == 0) return 100000;
 	    
-	    if(dp[n][w] != -1) return dp[n][w];
+	    if(dp[N][W] != -1) return dp[N][W];
 	    
-	   // if (n <= w && cost[n-1] != -1) {
-	   //     return dp[n][w] = Math.min(cost[n-1] + solve(cost, n, w - n), solve(cost, n - 1, w));
-	   // }
-	    
-	   // return dp[n][w] = solve(cost, n - 1, w);
-	    
-	    if(n <= w && cost[n - 1] != -1){
-	        int take = cost[n - 1] + solve(cost, n , w - n);
-	        int notTake = solve(cost, n - 1, w);
-	        return dp[n][w] = Math.min(take, notTake);
+	    if(N <= W && cost[N - 1] != -1){
+	        int take = cost[N - 1] + solve(cost, N , W - N);
+	        int notTake = solve(cost, N - 1, W);
+	        return dp[N][W] = Math.min(take, notTake);
 	    }else{
-	        return dp[n][w] = solve(cost, n - 1, w);
+	        return dp[N][W] = solve(cost, N - 1, W);
 	    }
 	    
 	}
