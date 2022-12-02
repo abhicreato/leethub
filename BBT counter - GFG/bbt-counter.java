2a7f94a1dc71cede999f3893abf1d0c6d05583d2
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2022-12-02 11:59:17 +0530

    Added solution - LeetHub

---
+++ BBT counter - GFG/bbt-counter.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int h = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.countBT(h));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static long dp[];
+    
+    static long countBT(int h){
+      
+      dp = new long[h + 1];
+      
+      Arrays.fill(dp,-1);
+      
+      return solve(h);
+        
+    }
+    
+    static long solve(int h){
+         // code here
+        if( h == 0 || h == 1 ) return 1;
+        
+        if(dp[h] != -1) return dp[h];
+        
+        long ans = solve(h - 1) * (2 * solve(h - 2) + solve(h - 1));
+        
+        return dp[h] = ans % (long) (1e9 + 7);
+    }
+}
