8c99e3734c25d7159d6aa029ee4f65a4c818c13e
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2023-01-01 20:43:48 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (89.74%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -2,41 +2,15 @@ class Solution {
     public int search(int[] nums, int target) {
     
         int l = nums.length;
-        int start = 0;
-        int end = l - 1;
         
-        int minIdx = 0;
+        int minIdx = findMin(nums, l);
         
-        while(start <= end){
-            
-            int mid = (start + end)/2;
-            
-            if(nums[minIdx] == target){
-                return minIdx;
-            }
-            
-            int pre = (mid + l - 1) % l;
-            int next = (mid + 1) % l;
-            
-            if(nums[pre] >= nums[mid] && nums[next] >= nums[mid]){
-                minIdx = mid;
-                break;
-            }
-            if(nums[start] <= nums[end]){
-                minIdx = start;
-                break;
-            }
-            if(nums[mid] >= nums[start]){
-                start = mid + 1;
-            }
-            if(nums[mid] <= nums[end]){
-                end = mid - 1;
-            }
-            
+        if(nums[minIdx] == target){
+            return minIdx;
         }
-        
+            
         int left = bs(nums, 0, minIdx - 1, target);
-        int right = bs(nums, minIdx, l-1, target);
+        int right = bs(nums, minIdx, l - 1, target);
                 
         if(left!=-1){
             return left;
@@ -62,6 +36,38 @@ class Solution {
         }
         
         return - 1;
+    }
+    
+    int findMin(int nums[],int l){
+        
+        int start = 0;
+        int end = l - 1;
+        
+        int minIdx = 0;
+        
+        while(start <= end){
+            
+            int mid = (start + end)/2;
+            int pre = (mid + l - 1) % l;
+            int next = (mid + 1) % l;
+            
+            if(nums[pre] >= nums[mid] && nums[next] >= nums[mid]){
+                return mid;
+            }
+            if(nums[start] <= nums[end]){
+                return start;
+            }
+            if(nums[mid] >= nums[start]){
+                start = mid + 1;
+            }
+            if(nums[mid] <= nums[end]){
+                end = mid - 1;
+            }
+            
+        }
+        
+        return 0;
+        
     }
     
   
