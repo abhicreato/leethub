a2a0d01567bc2b19fb96450ded36a7d9e0bf4cc6
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2023-01-01 20:25:24 +0530

    Time: 2 ms (6.98%), Space: 41.9 MB (79.51%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,70 @@
+class Solution {
+    public int search(int[] nums, int target) {
+    
+        int l = nums.length;
+        int start = 0;
+        int end = l - 1;
+        
+        int minIdx = 0;
+        
+        while(start <= end){
+            
+            int mid = (start + end)/2;
+            
+            int pre = (mid + l - 1) % l;
+            int next = (mid + 1) % l;
+            
+            if(nums[pre] >= nums[mid] && nums[next] >= nums[mid]){
+                minIdx = mid;
+                break;
+            }
+            if(nums[start] <= nums[end]){
+                minIdx = start;
+                break;
+            }
+            if(nums[mid] >= nums[start]){
+                start = mid + 1;
+            }
+            if(nums[mid] <= nums[end]){
+                end = mid - 1;
+            }
+            
+        }
+        
+        System.out.println(minIdx);
+        
+        if(nums[minIdx] == target){
+            return minIdx;
+        }
+        
+        int left = bs(nums, 0, minIdx - 1, target);
+        int right = bs(nums, minIdx, l-1, target);
+                
+        if(left!=-1){
+            return left;
+        }if(right!=-1){
+            return right;
+        }
+        return -1;
+
+    }
+    
+    int bs(int arr[],int s,int e,int key){
+
+        while(s<=e){
+            
+            int mid = (s+e)/2;
+            
+            if(arr[mid] == key){
+                return mid;
+            }
+            
+            if(arr[mid]>key) e = mid - 1;
+            else s = mid + 1;
+        }
+        
+        return - 1;
+    }
+    
+  
+}
