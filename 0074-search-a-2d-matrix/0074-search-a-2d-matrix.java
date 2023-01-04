f358368285c90be72812b52152588d54ae4c5f04
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2023-01-04 10:27:25 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (34.01%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,20 +1,41 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i = 0;
-        int j = matrix[0].length-1;
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int s = 0;
+        int e = r - 1;
         
-        while (i>=0 && i<matrix.length && j>=0 && j<matrix[0].length){
-            if(matrix[i][j] == target){
-                return true;
+        while(s<=e){
+            
+            int mid = (s+e)/2;
+            
+            if(target >= matrix[mid][0] && target <= matrix[mid][c-1]){
+                return binarySeach(matrix[mid],target);
             }
-            else if(matrix[i][j] < target){
-                i++;
-            }else if(matrix[i][j] > target){
-                j--;
-            }
+            
+            if(target < matrix[mid][0]) e = mid - 1;
+            else s = mid + 1;
+            
         }
         
         return false;
+        
+        
+//         int i = 0;
+//         int j = matrix[0].length-1;
+        
+//         while (i>=0 && i<matrix.length && j>=0 && j<matrix[0].length){
+//             if(matrix[i][j] == target){
+//                 return true;
+//             }
+//             else if(matrix[i][j] < target){
+//                 i++;
+//             }else if(matrix[i][j] > target){
+//                 j--;
+//             }
+//         }
+        
+//         return false;
         
 //         for(int i = 0 ;i<matrix.length;i++){
 //             for(int j = 0; j<matrix[0].length;j++){
@@ -25,6 +46,30 @@ class Solution {
 //         }
         
        // return false;
+        
+    }
+    
+    
+    public boolean binarySeach(int[] nums, int target){
+        
+        int l = nums.length;
+        int s = 0;
+        int e = l - 1;
+        
+        while(s<=e){
+            
+            int mid = (s+e)/2;
+            
+            if(nums[mid] == target){
+                return true;
+            }
+            
+            if(nums[mid] > target) e = mid - 1;
+            else s = mid + 1;
+            
+        }
+        
+        return false;
         
     }
 }
