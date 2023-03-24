f2fae2838dbff372c638acbc1d1407f7c1c7e500
Abhishek Pawaskar <70562693+abhicreato@users.noreply.github.com>
2023-03-24 11:51:25 +0530

    Time: 59 ms (66.65%), Space: 50.9 MB (15.46%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,40 +1,59 @@
 class BrowserHistory {
     
-    Stack<String> curr = new Stack<>();
-    Stack<String> forw = new Stack<>();
+    DoublyLinkedList history;
 
     public BrowserHistory(String homepage) {
-        curr.push(homepage);
+        
+        this.history = new DoublyLinkedList(homepage);
+        
     }
     
     public void visit(String url) {
         
-        while(!forw.empty()){
-            forw.pop();
-        }
+        DoublyLinkedList current = new DoublyLinkedList(url);
+        this.history.next = current;
+        current.prev = this.history;
+        this.history = current;
         
-        curr.push(url);
     }
     
     public String back(int steps) {
-
-        while(curr.size() > 1 && steps != 0){
-            forw.push(curr.pop());
+        DoublyLinkedList current = this.history;
+        while(steps > 0 && current.prev != null){
+            current = current.prev;
             steps--;
         }
         
-        return curr.peek();
+        this.history = current;
+        return current.data;
  
     }
     
     public String forward(int steps) {
         
-        while(!forw.empty() && steps != 0){
-            curr.push(forw.pop());
+        DoublyLinkedList current = this.history;
+        while(steps > 0 && current.next !=null){
+            current = current.next;
             steps--;
         }
+        this.history = current;
+        return current.data;
+
+    }
+}
+
+class DoublyLinkedList{
+    
+    String data;
+    DoublyLinkedList next;
+    DoublyLinkedList prev;
+    
+    DoublyLinkedList(){
         
-        return curr.peek();
+    }
+    
+    DoublyLinkedList(String data){
+        this.data = data;
     }
 }
 
